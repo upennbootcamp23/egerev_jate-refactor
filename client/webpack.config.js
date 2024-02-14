fe6d88df5eeb84a44997c16a0ddd056f1f71e2c7
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-14 09:40:58 -0500

    Added package.lock

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+//Configuring workbox plugins for both a service worker and manifest file
 module.exports = () => {
   return {
     mode: 'development',
@@ -45,6 +45,7 @@ module.exports = () => {
     ],
 
     //TODO: Add CSS loaders and babel to webpack.
+    //Adding CSS loaders and Babel to webpack.
     module: {
       rules: [
         {
