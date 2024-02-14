f0ec681646a633f7066ea8c94d2e536082e0b783
Alexander Egerev <alexandersegerev@gmail.com>
2024-02-14 09:54:11 -0500

    Updated src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// Implementing asset caching
+// Implementing assets
 registerRoute(
   ({request}) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
